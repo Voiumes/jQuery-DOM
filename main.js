16c13216b0916c397ca38b879a0c0abaa7d91ad4
Sean <omerta201@yahoo.com>
2018-08-10 20:42:23 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+$("input").keypress(function(){
+    if(event.which === 13){
+        let todo = $(this).val()
+        $("ul").append('<li><span><i class="far fa-trash-alt"></i></span> ' + todo + '</li>');
+        $(this).val('')
+    }
+})
+
+$("ul").on("click", "span", function(){
+    $(this).parent().fadeOut(400, function(){
+       $(this).remove();
+    })
+})
+$("ul").on("click", "li", function(){
+    $(this).toggleClass('strike');
+})
