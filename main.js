84665e9742f2416e719962c1f33d6c0d0957941d
Sean <omerta201@yahoo.com>
2018-08-16 17:54:45 -0400

    e

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ $("ul").on("click", "span", function(){
 $("ul").on("click", "li", function(){
     $(this).toggleClass('strike');
 })
+
+document.querySelector('.scrollP5').addEventListener('click', function(e){
+    e.preventDefault;
+    document.querySelector("#p5").scrollIntoView({ behavior: 'smooth' }).scrollBy({ top: 500, left: 0, behavior: 'smooth' });
+   
+})
+document.querySelector('.scrollP4').addEventListener('click', function(e){
+    e.preventDefault;
+    document.querySelector("#p4").scrollIntoView({ behavior: 'smooth' });
+})
+document.querySelector('.scrollP3').addEventListener('click', function(e){
+    e.preventDefault;
+    document.querySelector("#p3").scrollIntoView({ behavior: 'smooth' });
+})
+
